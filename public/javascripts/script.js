e89e8a334afe7ba2493546a3ed9cfdf8f12ca8a0
Kyle Stengline <kyle.stengline@gmail.com>
2016-10-30 19:26:39 -0400

    replaces h1 with button on index, adds nav bar in welcome html

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   setTimeout(function() {
     $("#welcome_sign").fadeOut(2000); 
     setTimeout(function() {
-      var enter_text = $("#welcome_sign").text("Enter Here").css("color", "#E32924").fadeIn(1500);
+      var enter_text = $("#welcome_sign").text("ENTER HERE").css("color", "#222").fadeIn(1500);
       enter_text.css("font-size", "27px");
       // ends second setTimeout
     }, 1700);  
